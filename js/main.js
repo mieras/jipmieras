577e7499ea610fbb40c9c2c78126c052bf1ffa8f
mieras <mieras@gmail.com>
2023-03-01 09:14:38 +0100

    js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var Engine = Matter.Engine,
       element: document.body,
       engine: engine,
       options: {
-        wireframes: false,
+        wireframes: true,
         background: '#f4f4f4',
         width: width,
         height: height
